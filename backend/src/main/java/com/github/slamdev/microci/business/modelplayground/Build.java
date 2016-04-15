65a3e6ca195687c6001b65cb78cd3222ba22e7e4
slamdev <slam.ul73@gmail.com>
2016-04-15 21:13:38 +0200

    model samples

---
+++ backend/src/main/java/com/github/slamdev/microci/business/modelplayground/Build.java
@@ -0,0 +1,22 @@
+package com.github.slamdev.microci.business.modelplayground;
+
+public class Build {
+
+    private long id = 104448055;
+    private long repositoryId = 5724970;
+    private String repositorySlug = "slamdev/messages-watcher";
+    private long buildId = 104448054;
+    private long commitId = 29567382;
+    private long logId = 74967036;
+    private String state = "passed";
+    private String startedAt = "2016-01-24T15:24:26Z";
+    private String finishedAt = "2016-01-24T15:27:08Z";
+    private boolean allowFailure = false;
+    private long number = 14;
+    private String eventType = "push";
+    private boolean pullRequest = false;
+    private String pullRequestTitle = null;
+    private Long pullRequestNumber = null;
+    private int duration = 162;
+    private long[] job_ids = {104448055};
+}
---
+++ backend/src/main/java/com/github/slamdev/microci/business/modelplayground/Repo.java
@@ -0,0 +1,17 @@
+package com.github.slamdev.microci.business.modelplayground;
+
+public class Repo {
+
+    private long id = 5724970;
+    private String slug = "slamdev/messages-watcher";
+    private String description = "Platform that enables you to manage all of your communications from one place.";
+    private long lastBuildId = 104448054;
+    private long lastBuildNumber = 14;
+    private String lastBuildState = "passed";
+    private long lastBuildDuration = 162;
+    private String lastBuildLanguage = null;
+    private String lastBuildStartedAt = "2016-01-24T15:24:26Z";
+    private String lastBuildFinishedAt = "2016-01-24T15:27:08Z";
+    private boolean active = true;
+    private String githubLanguage = "Groovy";
+}
