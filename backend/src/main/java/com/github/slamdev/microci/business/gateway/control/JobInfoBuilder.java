d0db79c0a19b4937ace0b5af214bc2a35b60e1fe
slamdev <slam.ul73@gmail.com>
2016-04-25 12:44:30 +0200

    start experimenting with job builder

--- backend/src/main/java/com/github/slamdev/microci/business/gateway/control/JobInfoBuilder.java
+++ backend/src/main/java/com/github/slamdev/microci/business/gateway/control/JobInfoBuilder.java
@@ -3,12 +3,13 @@ package com.github.slamdev.microci.business.gateway.control;
 import com.github.slamdev.microci.business.gateway.entity.JobInfo;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class JobInfoBuilder {
 
     public List<JobInfo> buildAll() {
-        return null;
+        return Collections.emptyList();
     }
 }
